d9164306372b99ba3a49985c3a73f65563aa8dc0
Todd Mohney <toddmohney@gmail.com>
2017-12-17 20:40:20 -0500

    Add MonadMessageQueue instance to AppT

--- node-event-consumers/congress-consumer/app/App.hs
+++ node-event-consumers/congress-consumer/app/App.hs
@@ -5,13 +5,15 @@ module App
     ) where
 
 import           Control.Monad.Catch (MonadCatch, MonadThrow)
-import           Control.Monad.Except   (ExceptT (..), MonadError, runExceptT)
+import           Control.Monad.Except   (ExceptT (..), MonadError, runExceptT, throwError)
 import           Control.Monad.IO.Class (MonadIO)
 import           Control.Monad.Logger   (LoggingT (..), MonadLogger)
-import           Control.Monad.Reader   (MonadReader, ReaderT (..), runReaderT)
+import           Control.Monad.Reader   (MonadReader, ReaderT (..), asks, runReaderT)
 import           Network.Kafka          (KafkaClientError)
 
 import           AppConfig (AppConfig(..), mkAppConfig)
+import           CoinCoinCoin.MessageQueue (MonadMessageQueue(..))
+import           CoinCoinCoin.MessageQueue.Adapters.Kafka (runKafkaT)
 import           CoinCoinCoin.Logging (runLogging)
 
 newtype AppT m a = AppT { unAppT :: ReaderT AppConfig (LoggingT (ExceptT KafkaClientError m)) a }
@@ -30,3 +32,25 @@ runAppT :: (MonadIO m) => AppConfig -> AppT m a -> m (Either KafkaClientError a)
 runAppT cfg appT =
     runExceptT . runLogging False (appEnv cfg) $
         runReaderT (unAppT appT) cfg
+
+instance MonadIO m => MonadMessageQueue (AppT m) where
+    produceMessages jobs = do
+        kState <- asks kafkaState
+        result <- runKafkaT kState (produceMessages jobs)
+        case result of
+            Left err -> throwError err
+            Right resps -> return resps
+
+    getEarliestOffset kTopic kPartition = do
+        kState <- asks kafkaState
+        result <- runKafkaT kState (getEarliestOffset kTopic kPartition)
+        case result of
+            Left err -> throwError err
+            Right offset -> return offset
+
+    consumeMessages kTopic kPartition offset = do
+        kState <- asks kafkaState
+        result <- runKafkaT kState (consumeMessages kTopic kPartition offset)
+        case result of
+            Left err -> throwError err
+            Right resp -> return resp
