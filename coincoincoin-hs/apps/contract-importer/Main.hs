38a0c15d7984be30fb5680ae76ecae5d131fa28a
Todd Mohney <toddmohney@gmail.com>
2018-01-05 11:01:22 -0500

    Load build artifact files

--- coincoincoin-hs/apps/contract-importer/Main.hs
+++ coincoincoin-hs/apps/contract-importer/Main.hs
@@ -1,9 +1,18 @@
 module Main where
 
-import AppConfig (loadConfig)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+
+import AppConfig (AppConfig(..), loadConfig)
 
 
 main :: IO ()
 main = do
     config <- loadConfig
+    buildArtifacts <- loadBuildArtifacts $ contractsPath config
     print config
+    print $ length buildArtifacts
+
+loadBuildArtifacts :: FilePath -> IO [ByteString]
+loadBuildArtifacts path =
+    (:[]) <$> BS.readFile path
