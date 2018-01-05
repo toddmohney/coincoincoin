8ef2e7f02f88b2f38f33facdf9046f905d9408f1
Todd Mohney <toddmohney@gmail.com>
2018-01-05 13:17:57 -0500

    Accept files or directories as input

--- coincoincoin-hs/apps/contract-importer/AppConfig.hs
+++ coincoincoin-hs/apps/contract-importer/AppConfig.hs
@@ -9,9 +9,8 @@ import Data.Monoid ((<>))
 import CoinCoinCoin.Database.Config as DB
 import CoinCoinCoin.Database.Models (ConnectionString)
 
-data AppConfig = AppConfig
-    { connectionString :: ConnectionString
-    , contractsPath :: FilePath
+newtype AppConfig = AppConfig
+    { contractsPath :: FilePath
     } deriving (Show)
 
 {- Load configuation information from either the environment
@@ -33,25 +32,10 @@ programOpts dbConn =
         )
 
 optsParser :: Maybe ConnectionString -> Parser AppConfig
-optsParser mConn = case mConn of
-    Nothing ->
-        AppConfig
-            <$> option auto connectionStringParser
-            <*> argument auto filepathParser
-    (Just conn) ->
-        AppConfig
-            <$> option auto
-                ( connectionStringParser
-                    <> showDefault
-                    <> value conn
-                )
-            <*> argument auto filepathParser
+optsParser _ =
+    AppConfig
+        <$> argument str filepathParser
 
     where
-        connectionStringParser =
-            long "connection-string"
-                <> short 'c'
-                <> help "Connection to Postgresql database"
-
         filepathParser =
             metavar "SOURCE"
--- coincoincoin-hs/apps/contract-importer/Main.hs
+++ coincoincoin-hs/apps/contract-importer/Main.hs
@@ -1,18 +1,33 @@
 module Main where
 
+import qualified Control.Monad as M
+import qualified Data.Aeson as AE
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
+import qualified Data.Either as E
+import qualified System.Directory as D
+import qualified System.FilePath as FP
+
+import Truffle.Types (BuildArtifact)
 
 import AppConfig (AppConfig(..), loadConfig)
 
+data FileType = File | Directory
 
 main :: IO ()
 main = do
     config <- loadConfig
-    buildArtifacts <- loadBuildArtifacts $ contractsPath config
+    buildArtifacts <- fmap AE.eitherDecodeStrict <$> loadBuildArtifacts (contractsPath config) :: IO [Either String BuildArtifact]
     print config
-    print $ length buildArtifacts
+    print . length $ E.lefts buildArtifacts
+    print . length $ E.rights buildArtifacts
 
 loadBuildArtifacts :: FilePath -> IO [ByteString]
-loadBuildArtifacts path =
-    (:[]) <$> BS.readFile path
+loadBuildArtifacts path = do
+    isDir <- D.doesDirectoryExist path
+    if isDir
+    then do
+        files <- fmap (path FP.</>) <$> D.listDirectory path
+        M.foldM (\acc f -> (acc ++) <$> loadBuildArtifacts f) [] files
+    else
+        (:[]) <$> BS.readFile path
