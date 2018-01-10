f3385e76ff5768007896bf4c2f6ccfcc51178567
Todd Mohney <toddmohney@gmail.com>
2018-01-10 13:57:58 -0500

    Add readFilesRecursively to MonadFileReader interface

--- coincoincoin-hs/src/CoinCoinCoin/Class.hs
+++ coincoincoin-hs/src/CoinCoinCoin/Class.hs
@@ -6,11 +6,16 @@ module CoinCoinCoin.Class
     , UTCTime
     ) where
 
+import qualified Control.Concurrent.Async as A
+import qualified Control.Monad as M
+import           Control.Monad.IO.Class (MonadIO, liftIO)
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import           Data.Time.Clock (UTCTime)
 import qualified Data.Time.Clock as Time
+import           Prelude hiding (readFile)
 import qualified System.Directory as D
+import qualified System.FilePath as FP
 
 class (Monad m) => MonadTime m where
     getCurrentTime :: m UTCTime
@@ -18,12 +23,22 @@ class (Monad m) => MonadTime m where
 instance MonadTime IO where
     getCurrentTime = Time.getCurrentTime
 
-class (Monad m) => MonadFileReader m where
+class (MonadIO m) => MonadFileReader m where
     doesDirectoryExist :: FilePath -> m Bool
 
     listDirectory :: FilePath -> m [FilePath]
 
     readFile :: FilePath -> m ByteString
+
+    readFilesRecursively :: FilePath -> m [ByteString]
+    readFilesRecursively path = do
+        isDir <- doesDirectoryExist path
+        if isDir
+        then do
+            files <- fmap (path FP.</>) <$> listDirectory path
+            M.join <$> liftIO (A.mapConcurrently readFilesRecursively files)
+        else
+            (:[]) <$> readFile path
 
 instance MonadFileReader IO where
     doesDirectoryExist :: FilePath -> IO Bool
