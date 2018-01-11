b77bef03fa6a1ddc9cd15b8249218736e40f06ef
Todd Mohney <toddmohney@gmail.com>
2018-01-11 13:39:35 -0500

    Add endpoint providing contract data

--- hello/app.rb
+++ hello/app.rb
@@ -1,5 +1,21 @@
+require 'sequel'
 require 'sinatra'
+
+DB = Sequel.connect('postgres://coincoincoin:coincoincoin@localhost:5432/coincoincoin')
+
+class Contract < Sequel::Model
+  def to_json
+    values.
+      merge("abi" => JSON.parse(values.fetch(:abi))).
+      to_json
+  end
+end
 
 get '/' do
   File.read('./public/index.html')
+end
+
+get '/contracts' do
+  contracts = Contract.where(name: "Congress", network_id: "15")
+  contracts.map(&:to_json)
 end
